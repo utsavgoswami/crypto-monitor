ddeafafefd05bcaf2fc6ba110b4f66466e447bc6
utsavgoswami <utsavg19@googlemail.com>
2024-09-08 14:35:47 -0400

    Fix lint errs

--- crypto-monitor/vite.config.ts
+++ crypto-monitor/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
