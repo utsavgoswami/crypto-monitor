725f0278cd76938ce8bcad447578125c3701db51
utsavgoswami <utsavg19@googlemail.com>
2024-09-08 14:24:36 -0400

    Modify config

--- crypto-monitor/vite.config.ts
+++ crypto-monitor/vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {
-      VITE_UPSTASH_REDIS_REST_URL: process.env.VITE_UPSTASH_REDIS_REST_URL,
-      VITE_UPSTASH_REDIS_REST_TOKEN: process.env.VITE_UPSTASH_REDIS_REST_TOKEN,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env,
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-})
+  };
+});
