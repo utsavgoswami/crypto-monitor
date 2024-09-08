53431731d061662e56f7573bffc2c94af6869c94
utsavgoswami <utsavg19@googlemail.com>
2024-09-08 14:34:39 -0400

    Use process env polyfill technique

--- crypto-monitor/vite.config.ts
+++ crypto-monitor/vite.config.ts
@@ -5,11 +5,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  // const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     define: {
-      'process.env': env,
+      'process.env': process.env,
     },
     resolve: {
       alias: {
