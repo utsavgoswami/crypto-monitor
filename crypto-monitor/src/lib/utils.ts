2e35fd148eaad532f7794e52d69a6e4ef0fbea36
utsavgoswami <utsavg19@googlemail.com>
2024-09-08 12:17:24 -0400

    Impl util fns for getting cache key

--- crypto-monitor/src/lib/utils.ts
+++ crypto-monitor/src/lib/utils.ts
@@ -8,3 +8,16 @@ export function cn(...inputs: ClassValue[]) {
 export function toDateWithoutTime(date: Date): Date {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate());
 }
+
+export function getDateOneYearBefore(date: Date): Date {
+  const oneYearBefore = new Date(date);
+  oneYearBefore.setFullYear(date.getFullYear() - 1);
+  return oneYearBefore;
+}
+
+export const getCacheKey = (coinId: string) => {
+  const today = toDateWithoutTime(new Date());
+  const oneYearBefore = getDateOneYearBefore(today);
+
+  return `${coinId}-${oneYearBefore.getTime()}-${today.getTime()}`;
+}
