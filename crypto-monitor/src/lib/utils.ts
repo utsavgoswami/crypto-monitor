02206f97ad1ca56ba296fe931cef8a7bf2b8295d
utsavgoswami <utsavg19@googlemail.com>
2024-09-08 11:46:18 -0400

    Install Upstash TS SDK

--- crypto-monitor/src/lib/utils.ts
+++ crypto-monitor/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function toDateWithoutTime(date: Date): Date {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+}
